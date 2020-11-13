71385d4d227b3c448d94114b32da54be8b91f06c
Ray Stylee <936545016@qq.com>
2020-11-13 19:30:36 +0800

    revise sv and ld

--- R/io.R
+++ R/io.R
@@ -2,15 +2,15 @@
 #'
 #' @export
 #' @param objname expression or char. The object to be saved, use its R object name, e.g., sv(dt).
-#' @param filename expression or char. The file name of the serialized object. Default to objname, e.g., sv(dt, dt_new_name)
+#' @param svname expression or char. The file name of the serialized object. Default to objname, e.g., sv(dt, dt_new_name)
 #' @param compress Whether the object should be compressed
 #' @param path To which subfolder the data is saved. Default to '../data'. If './', save to current directory. 
-#' @param rds Whether the object should be saved as '.rds' file. The default value is 'TRUE', which the object will be saved as '.Rdata' file.
+#' @param svtype The extension of saved file. Currently only `Rdata`, `rds` and `feather` are available. Default to `rds`. 
 #'
 #' @examples
 #' sv(dt) # equals to sv('dt')
 #' sv(dt, dt_new_name, path='../Rdata')
-sv <- function(objname, filename=NULL, path = "./data", compress = T, rds = T) {
+sv <- function(objname, svname=NULL, svtype = 'Rdata', path = "./data", compress = T) {
     
     start <- Sys.time()
 
@@ -24,96 +24,121 @@ sv <- function(objname, filename=NULL, path = "./data", compress = T, rds = T) {
     if (is.null(substitute(filename))) {
         filename = as.character(substitute(objname))
     }
-    svdir <- c(
-                paste0(path, '/', substitute(filename), ".rds"), 
-                paste0(path, '/', substitute(filename), ".Rdata")
-                )
+    svdir <- paste0(path, '/', substitute(svname), '.', svtype)
 
     # serialize with `saveRDS` or `save`
-    if (rds == T) {
-        saveRDS(objname, file = svdir[1], compress = compress)
-    } else {
-        save(list = deparse(substitute(objname)), file = svdir[2], compress = compress, compression_level = 3)
+    if (svtype == 'Rdata') {
+        save(list = deparse(substitute(objname)), file = svdir, compress = compress, compression_level = 3)
+    } else if (svtype == "rds"){
+        saveRDS(objname, file = svdir, compress = compress)
+    } else if (svtype == "feather") {
+        feather :: write_feather(objname, svdir)
     }
-    
-    cat(str_c("-", substitute(filename), "- saved  "))
+    cat(stringr::str_c("-", substitute(svname), "- saved  "))
     
     end <- Sys.time()
     gap <- end - start
-    cat(str_c("(", round(gap, 2), ' ', units(gap), ")\n"))
+    cat(stringr::str_c("(", round(gap, 2), ' ', units(gap), ")\n"))
 }
 
 
-#' A simplified load function using readRDS() or load().
+#' A simplified load function using readRDS(), load() or read_feather().
 #'
 #' @export
 #' @param filename expression or char. The file name to be loaded, e.g. ld(x) equals to load(file='dt.rds')
-#' @param objname expression or char. The object name in R. If NULL then equals to filename
+#' @param objname expression or char. The object name in R. If NULL then equals to filename.
 #' @param force Whether the object should be reloaded if it's already in the current environment.
 #' @param path From which subfolder to read the data. Default to '../data'. If './', load from current directory.
-#' @param rds Whether the object should be load from '.rds' file. The default value is 'FALSE', which the object will be load from '.Rdata' file.
+#' @param ldtype `Rdata`, `rds` or `feather`. The default is `Rdata`.
 #'
 #' @examples
 #' ld(dt)
 #' ld(filename=dt, objname=dt_new, path='../Rdata')
-ld <- function(filename, objname=NULL, path = './data', force = F, rds = F) {
+ld <- function(filename, objname=NULL, ldtype = 'Rdata', path = './data', force = F) {
     start <- Sys.time()
 
     # create load dir
-    lddir <- c(paste0(path, '/', substitute(filename), ".rds"), paste0(path, '/', substitute(filename), ".Rdata"))
+    lddir <- paste0(path, '/', substitute(filename), ".", ldtype)
 
     # check if the file exists
-    if (sum(file.exists(lddir)) == 0) {
-        stop("Object not exists!")
+    if (!file.exists(lddir)) {
+        stop("Object not exists!", lddir)
     }
-    # create a function list containing `readRDS` and `load`
-    importfunc <- function(rds = rds) {
-        if (rds == F) {
-            load(lddir[2], env = .GlobalEnv)
-        } else {
-            readRDS(lddir[1])
-        }
-    }
-
 
     # load data
     if (force == F) {
         if (is.null(substitute(objname))) { # if objname is null
             # if filename not exist in global, load
             if (!exists(as.character(substitute(filename)))) {
-                val = importfunc
-                assign(as.character(substitute(filename)), val, env=.GlobalEnv)
-                cat(str_c("-", substitute(filename), "- loaded"))
-            } else {
+                if (ldtype == 'Rdata') {
+                    load(lddir, envir = .GlobalEnv)
+                } 
+                else {
+                    if (ldtype == 'rds') {
+                        val = readRDS(lddir)
+                    }
+                    else if (ldtype == 'feather') {
+                        val = feather::read_feather(lddir) %>% data.table::setDT()
+                    }
+                    assign(as.character(substitute(filename)), val, env=.GlobalEnv)
+                }
+                
+                cat(paste0("-", substitute(filename), "- loaded"))
+            } 
+            else {
                 # if filename exist, Not load
-                cat(str_c("-", substitute(filename), "- already exists, will NOT load again!"))
+                cat(paste0("-", substitute(filename), "- already exists, will NOT load again!"))
             }
-        } else { # if objname isn't null
+        } 
+        else { # if objname isn't null
             # if objname not exist in global, load
             if (!exists(as.character(substitute(objname)))) {
-                val = importfunc
-                assign(as.character(substitute(objname)), val, env=.GlobalEnv)
-                cat(str_c("-", substitute(filename), "- loaded as -", substitute(objname), "-"))
-            } else {
+                if (ldtype == 'Rdata') {
+                    load(lddir, envir = .GlobalEnv)
+                }
+                else {
+                    if (ldtype == 'rds') {
+                        val = readRDS(lddir)
+                    }
+                    else if (ldtype == 'feather') {
+                        val = feather::read_feather(lddir) %>% data.table::setDT()
+                    }
+                    assign(as.character(substitute(filename)), val, env = .GlobalEnv)
+                }
+                cat(paste0("-", substitute(filename), "- loaded as -", substitute(objname), "-"))
+
+            } 
+            else {
                 # if objname exist in global, Not load
-                cat(str_c("-", substitute(objname), "- already exists, will NOT load again!"))
+                cat(paste0("-", substitute(objname), "- already exists, will NOT load again!"))
             }
         }
-    } else if (force == T) {
-        val = importfunc
+    } 
+    else if (force == T) {
         if (is.null(substitute(objname))) {
-            assign(as.character(substitute(filename)), val, env=.GlobalEnv)
-            cat(str_c("-", substitute(filename), "- loaded"))
+            if (ldtype == 'Rdata') {
+                load(lddir, envir = .GlobalEnv)
+            }
+            else {
+                if (ldtype == 'rds') {
+                    val = readRDS(lddir)
+                }
+                else if (ldtype == 'feather') {
+                    val = feather::read_feather(lddir) %>% data.table::setDT()
+                }
+                assign(as.character(substitute(filename)), val, env = .GlobalEnv)
+            }
+            cat(paste0("-", substitute(filename), "- loaded"))
         } else {
-            assign(as.character(substitute(objname)), val, env=.GlobalEnv)
-            cat(str_c("-", substitute(filename), "- loaded as -", substitute(objname), "-"))
+            #assign(as.character(substitute(objname)), val, env=.GlobalEnv)
+            cat(paste0("-", substitute(filename), "- loaded as -", substitute(objname), "-"))
         }
     }
         
     # output time elapsed
     end <- Sys.time()
     gap <- end - start
-    cat(str_c("  (", round(gap, 2), ' ', units(gap), ")\n"))
+    cat(paste0("  (", round(gap, 2), ' ', units(gap), ")\n"))
 }
 
 #' A simplified read batches function using fread().
